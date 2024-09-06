aacacec10e8313b7d9edc02eac94578ab187d816
hilmanhaikal <hilmanhaikal7@gmail.com>
2024-09-06 08:45:40 +0800

    update files

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="hero" class="text-gray-950 text-center py-16 px-4">
     <h1 class="text-4xl font-bold">Hi, I'm Hilman Haikal</h1>
-    <p class="text-xl mt-4">A passionate software and mobile developer.</p>
+    <p class="text-xl mt-4">A software and mobile developer, casting digital spells with my keyboard wand.</p>
     <a href="#projects" class="hover:bg-gray-500 hover:text-gray-950 transition-colors duration-200 mt-8 inline-block bg-gray-800 text-gray-100 py-2 px-4 rounded-lg">See my work</a>
   </section>
 </template>
