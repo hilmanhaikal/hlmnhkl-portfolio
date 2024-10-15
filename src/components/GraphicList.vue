6f70e83e2a7aaee2a0da46cbc7aa8c81196ab1ee
hilmanhaikal <hilmanhaikal7@gmail.com>
2024-10-16 07:05:25 +0800

    update graphiclist.vue

--- src/components/GraphicList.vue
+++ src/components/GraphicList.vue
@@ -21,6 +21,23 @@
               @click.prevent="openModal('/images/familytrip-1.png')"
             >View Design</a>
           </div>
+
+          <div class="flex flex-col p-6 rounded-lg shadow-md bg-gray-100">
+            <h3 class="text-xl font-semibold">Logo Design</h3>
+            <img
+              src="/images/animationlogo.png"
+              alt="Logo Design"
+              class="block mx-auto w-auto h-auto mt-4 rounded-lg shadow-md"
+            />
+            <p class="flex-grow mt-2 text-justify">
+                "Unlimited Music Records" logo bursts with cosmic energy, embodying the boundless creativity of the brand. The bold design and vibrant color scheme represent our commitment to pushing musical boundaries and exploring uncharted sonic territories.
+            </p>
+            <a
+              href="#"
+              class="hover:bg-gray-500 hover:text-gray-950 transition-colors duration-200 mt-4 inline-block bg-gray-800 text-white py-2 px-4 rounded-lg self-start"
+              @click.prevent="openModal('/images/animationlogo.png')"
+            >View Design</a>
+          </div>
   
           <!-- Another Graphic Design Project -->
           <div class="flex flex-col p-6 rounded-lg shadow-md bg-gray-100">
