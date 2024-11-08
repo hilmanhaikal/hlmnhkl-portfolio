f98be20eb2ff395b488dd553e247400abbd8268b
hilmanhaikal <hilmanhaikal7@gmail.com>
2024-11-08 21:29:25 +0800

    add new design project

--- src/components/GraphicList.vue
+++ src/components/GraphicList.vue
@@ -5,6 +5,23 @@
         <!-- Grid layout for graphic design projects -->
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
           <!-- Example Graphic Design Project -->
+           <div class="flex flex-col p-6 rounded-lg shadow-md bg-gray-100">
+            <h3 class="text-xl font-semibold">Logo Design</h3>
+            <img
+              src="/images/first_design_logo.jpg"
+              alt="Logo Design"
+              class="block mx-auto w-auto h-auto mt-4 rounded-lg shadow-md"
+            />
+            <p class="flex-grow mt-2 text-justify">
+                An upcoming business logo design
+            </p>
+            <a
+              href="#"
+              class="hover:bg-gray-500 hover:text-gray-950 transition-colors duration-200 mt-4 inline-block bg-gray-800 text-white py-2 px-4 rounded-lg self-start"
+              @click.prevent="openModal('/images/familytrip-1.png')"
+            >View Design</a>
+
+          </div>
           <div class="flex flex-col p-6 rounded-lg shadow-md bg-gray-100">
             <h3 class="text-xl font-semibold">Logo Design</h3>
             <img
