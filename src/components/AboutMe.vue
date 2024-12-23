859f1948bc7758da6535ef7434c94b72224639d1
hilmanhaikal <hilmanhaikal7@gmail.com>
2024-12-23 17:19:50 +0800

    update AboutMe.vue

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -12,12 +12,9 @@
       <div class="mt-10 text-center">
         <h3 class="text-3xl font-semibold">Skills</h3>
         <ul class="list-none mt-4 inline-block max-w-3xl mx-auto leading-relaxed text-center space-y-2">
-          <li class="text-lg">HTML, CSS, Tailwind CSS, JavaScript, Vue.js, Python, Flutter, Dart, Google Firebase, Github, JSON</li>
-          <li class="text-lg">Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro, Adobe After
-            Effect, Figma, Microsoft Word, Microsoft Powerpoint.
-          </li>
-          <li class="text-lg">Mobile Development, Software Development, Front End Development, Agile Methodology, Problem Solving, Creative Thinking,
-            Team Player, Research and Analysis, Team Collaboration, Innovative Solutions.</li>
+          <li class="text-lg">HTML, CSS, Tailwind CSS, JavaScript, Vue.js, Python, Flutter, Dart, Google Firebase, Github, JSON, Laravel.</li>
+          <li class="text-lg">Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro, Adobe After Effect, Figma, Microsoft Word, Microsoft Powerpoint.</li>
+          <li class="text-lg">Mobile Development, Software Development, Front End Development, Agile Methodology, Problem Solving, Creative Thinking, Team Player, Research and Analysis, Team Collaboration, Innovative Solutions.</li>
         </ul>
       </div>
     </div>
