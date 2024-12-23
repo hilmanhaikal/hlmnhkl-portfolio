752f490f5d4950a75c448567c5d27f08bfba495a
hilmanhaikal <hilmanhaikal7@gmail.com>
2024-12-23 16:56:00 +0800

    update AboutMe.vue

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -25,7 +25,7 @@
           href="https://drive.google.com/uc?export=download&id=1dEMvmvLgonyQC5728pTw5ZkU7HYNB88R"
           class="hover:bg-gray-500 hover:text-gray-950 transition-colors duration-200 mt-8 inline-block bg-gray-800 text-white py-2 px-4 rounded-lg"
         >
-          Download CV
+          Hire Me
         </a>
       </div>
     </div>
